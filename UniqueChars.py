8bceef7104afaa6d3105b7041cf317253cd1a49a
Ishu9581 <130961999+Ishu9581@users.noreply.github.com>
2023-04-30 10:35:33 +0530

    Update UniqueChars.py

--- UniqueChars.py
+++ UniqueChars.py
@@ -1,7 +1,10 @@
-s=input().lower()
+#Description: Write a program in Python to print the number of unique letters in a string. Only new letters from the string should be counted and not duplicates.
+
+s=input("Enter a String:").lower()
 l=[]
 for i in s:
 	if i in l:
 		continue;
 	l.append(i)
+print("Unique Letters:",end=" ")
 print(",".join(l))
